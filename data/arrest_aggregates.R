149459fc22dab6bd1ba689b676b8ec0abaadad2d
Tim Dennis <dennis.tim@gmail.com>
2019-11-17 18:14:40 -0800

    adding lesson 4

--- data/arrest_aggregates.R
+++ data/arrest_aggregates.R
@@ -8,6 +8,7 @@ library(tidyverse)
 library(sf)
 library(janitor)
 library(lubridate)
+library(tmap)
 
 arrests17_18 <- read_csv('data/ARREST_DATA_2017_2018.csv')
 arrests17_18 <- clean_names(arrests17_18)
@@ -15,4 +16,39 @@ names(arrests17_18)
 
 arrests_zip <- arrests17_18 %>%
   group_by(zipcode) %>%
-  tally()
+  tally() %>%
+  ungroup()
+
+#geometry type:  MULTIPOLYGON
+la_zips <- st_read(dsn = "data/Los_Angeles_City_Zip_Codes/Los_Angeles_City_Zip_Codes.shp")
+la_zips <- clean_names(la_zips)
+
+#merging our arrests data + zip shapes
+arrest_zip <- left_join(arrests_zip, la_zips, by = c("zipcode" = "zip"))
+
+#writing out zip
+write_sf(obj = arrest_zip, dsn = "data/arrests_zip.gpkg")
+
+# reading in zip data
+arrestsbyzip <- st_read(dsn = "data/arrests_zip.gpkg")
+
+tmap_mode("view")
+#map of arrests
+tm_shape(arrestsbyzip ) +
+  tm_polygons("n")
+
+st_is_empty(arrestsbyzip)
+
+arrests17_18 %>%
+  select(charge, charge_desc) %>%
+  count(charge, charge_desc, sort=TRUE) %>%
+  View()
+
+arrests_10_18 <- read_csv('data/Arrest_Data_from_2010_to_Present.csv')
+arrests_10_18 <- clean_names(arrests_10_18)
+glimpse(arrests_10_18)
+max(arrests_10_18$arrest_date)
+min(arrests_10_18$arrest_date)
+
+
+
